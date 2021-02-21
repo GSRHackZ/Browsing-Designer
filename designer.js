76b43014306db0dfe35a69dea1c0bbd6acd8a3d5
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2021-02-21 13:10:49 -0500

    Update designer.js
    Made rainbow mode colors brighter 🌈

--- designer.js
+++ designer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Browsing Designer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Design and customize any website you visit!!
 // @author       GSRHackZ
 // @match         *://*/*
@@ -109,9 +109,5 @@ if(localStorage.getItem("txtFont")!==null){
 
 
 function randomHSL(){
-    return "hsla(" + ~~(360 * Math.random()) + "," +
-        "70%,"+
-        "80%,1)"
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
-
-
